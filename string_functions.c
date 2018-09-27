eddf8ac54d01df7d9bdbc82f19d7e4090075df39
aidris823 <pokeanand@gmail.com>
2018-09-27 11:01:15 -0400

    Started work.

---
+++ string_functions.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "assignment.h"
+
+int string_len(char string[]);
+char * string_copy(char* dest, char *source);
+char * string_cat(char* dest, char *source);
+int * string_cmp(char *s1, char *s2);
+char string_chr(char *s, char c);
+
+char *string_copy(char *dest, char *source){
+  
+}
+int main(){
+}
+
