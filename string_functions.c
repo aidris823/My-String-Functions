1d560edb08713e268874de7bbeac0047ec88ca63
aidris823 <pokeanand@gmail.com>
2018-09-28 10:45:51 -0400

    What I have so far for string_len and string_chr.

--- string_functions.c
+++ string_functions.c
@@ -7,10 +7,25 @@ int string_len(char string[]);
 char * string_copy(char* dest, char *source);
 char * string_cat(char* dest, char *source);
 int * string_cmp(char *s1, char *s2);
-char string_chr(char *s, char c);
+char * string_chr(char *s, char c);
 
-char *string_copy(char *dest, char *source){
-  
+int string_len(char string[]){
+  int i = 0;
+  int ans = 0;
+  while (string[i] != 0){
+    ans++;
+  }
+  return ans;
+}
+
+char * string_chr(char *s, char c){
+  int i = 0;
+  while (s[i] != 0){
+    if (s[i] == c){
+      return *(s + i);
+    }
+  }
+  return NULL;
 }
 int main(){
 }
